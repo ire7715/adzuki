12fcb6d6bafa894acfafac9f7e34d4b425048a0a
ire7715 <ire7715@hotmail.com>
2019-03-24 15:45:06 +0100

    test case with wrong schema

--- tests/books/createbookssuite.py
+++ tests/books/createbookssuite.py
@@ -31,3 +31,32 @@ class CreateBooksSuite(AbstractTestBook):
     self.assertEqual(book_entity.id, self._ISBN)
     for key, value in expected_book_entity.items():
       self.assertEqual(book_entity[key], value)
+
+  def testCreateWrongSchema(self):
+    input_entity = {
+      'title': 'The Hobbit',
+      'language': 'English',
+      'pages': '320',
+      'author.name': 'J.R.R. Tolkien',
+      'released_at': '2007-09-17T00:00:00Z'
+    }
+
+    books_model = Book()
+    book_handler = books_model.create(self._ISBN)
+    for key, value in input_entity.items():
+      if key in ['author.name', 'released_at']:
+        with self.assertRaises(Exception):
+          book_handler[key] = value
+      else:
+        book_handler[key] = value
+    book_handler.put()
+
+    book_entity = books_model.get(self._ISBN)
+    self.assertEqual(book_entity.id, self._ISBN)
+    for key, value in input_entity.items():
+      if key in ['author.name', 'released_at']:
+        continue
+      elif key == 'pages':
+        self.assertEqual(book_entity[key], int(value))
+      else:
+        self.assertEqual(book_entity[key], value)
